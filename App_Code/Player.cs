d04bef99617bfdd8abfd52deb8fdbbe4a6e4ff49
AbidCharlotte49er <buddu.skabid@gmail.com>
2014-02-24 11:41:00 -0500

    LQ2SQL

---
+++ App_Code/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+
+/// <summary>
+/// this is a partial Player class main properties are coming from EDMX. 
+/// </summary>
+public partial class Player 
+{
+    public string ProfileTemplate = @"
+            <div class=""span3"">
+                <div class=""thumbnail"">
+                    <img class=""img-polaroid"" src=""{0}"" alt=""{1}"">
+                    <div class=""caption"">
+                        <h5>{1}</h5>
+                        <p>{2}</p>
+                    </div>
+                </div>
+            </div>"; 
+
+}
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,8 +8,16 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
+    LnqToSQLDataContext LQ2SQContext = new LnqToSQLDataContext(ConfigurationManager.ConnectionStrings["ashaik_TestConnectionString"].ConnectionString); 
     protected void Page_Load(object sender, EventArgs e)
     {
+        var ps = from p in LQ2SQContext.Players select p; 
 
+        foreach(var p in ps)
+        {
+            p.ProfileTemplate = string.Format(p.ProfileTemplate, "http://ashaiks.com/unc/img/apple-touch-icon-iphone4.png", p.FirstName + " " + p.LastName, p.Email);
+            DvRoster.InnerHtml += p.ProfileTemplate; 
+
+        }
     }
 }
